c32c70205807aa2ec94087164aaa1dbd0e758437
Walker <caidenwilson@protonmail.com>
2020-09-04 17:36:32 +0530

    update

--- August-2020/home/.config/fish/config.fish
+++ August-2020/home/.config/fish/config.fish
@@ -15,9 +15,9 @@ echo sometext | xclip
 
 
 # Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
-end
-
+# if status is-login
+    # if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        # exec startx -- -keeptty
+    # end
+# end
+# 
